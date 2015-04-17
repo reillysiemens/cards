309ed46ef8c555da51abd3884d8946f91f84040b
Reilly Tucker Siemens <reilly.siemens@gmail.com>
2015-04-17 14:40:24 -0700

    Add delete_cards method to cards.py.

--- cards.py
+++ cards.py
@@ -50,3 +50,17 @@ class Cards:
         keys = ['set', 'color', 'text', 'creator']
         filtered = [ { k: card[k] for k in keys if k in card} for card in cards]
         self.cards_coll.insert_many(filtered)
+
+    def delete_cards(self, filterdict):
+        """Delete all the cards matching a filter.
+
+        Args:
+          filterdict: Dictionary with set, color, text, and/or creator keys.
+
+        Returns:
+          None
+        """
+
+        keys = ['set', 'color', 'text', 'creator']
+        filterdict = { k: filterdict[k] for k in keys if k in filterdict }
+        self.cards_coll.delete_many(filterdict)
